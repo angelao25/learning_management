0ff52d487d6f84d15fa4cde36627b62b550daa35
Angel Aguirre Obando <anngelao25@gmail.com>
2024-06-13 00:47:14 -0500

    Added Categories and Lessons relation

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,4 +1,6 @@
 class Course < ApplicationRecord
+  has_many :lessons
+  has_and_belongs_to_many :categories
   has_one_attached :image do |attachable|
     attachable.variant :thumb, resize_to_limit: [100, 100]
   end
