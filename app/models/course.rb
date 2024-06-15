0fc4b0f7442bf409c42409a6572c0a8954d3832c
Angel Aguirre Obando <anngelao25@gmail.com>
2024-06-15 00:13:58 -0500

    Added new routes and confirmable module from devise to confirm emails

--- app/models/course.rb
+++ app/models/course.rb
@@ -4,4 +4,8 @@ class Course < ApplicationRecord
   has_one_attached :image do |attachable|
     attachable.variant :thumb, resize_to_limit: [100, 100]
   end
+
+  def first_lesson
+    lessons.order(:position).first
+  end
 end
